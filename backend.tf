efa6d8e656d11fe1e42a9c69639c77b5244532ab
lanchesterr <111203913+lanchesterr@users.noreply.github.com>
2026-04-18 22:18:35 +0200

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfstate1232137"
-    container_name       = "tfstate"
-    key                  = "dev/terraform.tfstate"
-  }
-}
